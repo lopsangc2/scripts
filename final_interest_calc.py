ecd018af0adc7f8ec094b01be0d084cc1f810350
lopsangc2 <lopsangc2@gmail.com>
2020-11-23 08:15:24 +0545

    finished adding user input

--- final_interest_calc.py
+++ final_interest_calc.py
@@ -1,6 +1,6 @@
-original_principal = 1000000
-rate_of_interest = 0.1
-time_interval = 25
+original_principal = float(input("Enter The Amount you want to deposit  "))
+rate_of_interest = float(input("Enter the rate of interest  "))
+time_interval = int(input("How many years would you like to deposit your Amount  "))
 times = time_interval + 1
 interest_yearly = []
 principal_yearly = []
@@ -8,7 +8,7 @@ principal_yearly = []
 def com_interset_calc(original_principal1,rate_of_interest2,time_interval3):
     for time in range(1,times):
         interest = int(original_principal1 * rate_of_interest2)
-        original_principal1 = original_principal1 + interest
+        original_principal1 = int(original_principal1 + interest)
         interest_yearly.append(interest)
         principal_yearly.append(original_principal1)
     #   print(interest)
@@ -21,8 +21,7 @@ def com_interset_calc(original_principal1,rate_of_interest2,time_interval3):
         print("your interest for " + str(year+1) + " year is " + str (interest_yearly[year])+ " and principal is "  +str(principal_yearly[year]))
         year += 1
 
-#com_interset_calc(original_principal,rate_of_interest,time_interval)
-com_interset_calc(1000000,0.2,10)
+com_interset_calc(original_principal,rate_of_interest,time_interval)
 
 #print(interest_yearly)
 #print(principal_yearly)
