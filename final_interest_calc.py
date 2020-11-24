02c0af1fb653af3326385133df5fa52bdb6f1303
lopsangc2 <lopsangc2@gmail.com>
2020-11-24 21:07:38 +0545

    making proper indentation

--- final_interest_calc.py
+++ final_interest_calc.py
@@ -1,6 +1,9 @@
-original_principal = float(input("Enter The Amount you want to deposit  "))
-rate_of_interest = float(input("Enter the rate of interest  "))
-time_interval = int(input("How many years would you like to deposit your Amount  "))
+original_principal = float(input("Enter The Amount " +
+                     "you want to deposit.  "))
+rate_of_interest = float(input("Enter the rate of "+
+                    "interest  "))
+time_interval = int(input("How many years would you "+ 
+                    "like to deposit your Amount  "))
 times = time_interval + 1
 interest_yearly = []
 principal_yearly = []
@@ -18,7 +21,10 @@ def com_interset_calc(original_principal1,rate_of_interest2,time_interval3):
     year = 0
 
     while year < time_interval3:    
-        print("your interest for " + str(year+1) + " year is " + str (interest_yearly[year])+ " and principal is "  +str(principal_yearly[year]))
+        print("your interest for " + str(year+1) + 
+        " year is " + str (interest_yearly[year])+
+         " and principal is "  
+         +str(principal_yearly[year]))
         year += 1
 
 com_interset_calc(original_principal,rate_of_interest,time_interval)
